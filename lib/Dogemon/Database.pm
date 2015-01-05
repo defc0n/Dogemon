43ba638505b8db5b840b2b1fd7c80744d72c9436
Mitch McCracken <mrmccrac@gmail.com>
2015-01-04 23:05:37 -0500

    get_nodes() get_services()

--- lib/Dogemon/Database.pm
+++ lib/Dogemon/Database.pm
@@ -3,14 +3,18 @@ package Dogemon::Database;
 use Moo;
 
 use AnyEvent;
-use AnyEvent::DBI;
+use AnyEvent::DBI::Abstract;
 
 use Data::Dumper;
 
-has filename => ( is => 'ro' );
+### attributes ###
+
+has filename => ( is => 'ro',
+		  required => 1 );
+
 has dbh => ( is => 'rw' );
 
-### init ###
+### initialization ###
 
 sub BUILD {
     
@@ -19,14 +23,97 @@ sub BUILD {
     my $filename = $self->filename;
     my $cv = AnyEvent->condvar;
 
-    my $dbh = AnyEvent::DBI->new( "DBI:SQLite:dbname=$filename", "", "",
-				  on_connect => sub { $cv->send },
-				  on_error => sub { warn @$ },
-				  fetch => sub { my ( $sth ) = @_; $sth->fetchall_arrayref( {} ) } );
+    my $dbh = AnyEvent::DBI::Abstract->new( "DBI:SQLite:dbname=$filename", "", "",
+					    on_connect => sub { $cv->send },
+					    on_error => sub { warn @$ } );
     
     $cv->recv;
 
     $self->dbh( $dbh );
+}
+
+### public methods ###
+
+sub get_nodes {
+
+    my ( $self, %args ) = @_;
+
+    my $table = 'node';
+    my $fields = ['node.node_id',
+		  'node.name',
+		  'node.ip_address'];
+    my $where = [];
+    my $order = ['node.node_id'];
+
+    my $results = [];
+
+    my $cv = AnyEvent->condvar;
+
+    $self->dbh->select( $table,
+			$fields,
+                        $where,
+			$order,
+			sub {
+
+                            my ( $dbh, $rows, $rv ) = @_;
+
+                            foreach my $row ( @$rows ) {
+
+				push $results, {'node_id' => $row->[0],
+                                                'name' => $row->[1],
+                                                'ip_address' => $row->[2]};
+                            }
+
+                            $cv->send;
+			} );
+
+    $cv->recv;
+
+    return $results;
+}
+
+sub get_services {
+
+    my ( $self, %args ) = @_;
+
+    my $table = 'service',
+    my $fields = ['service.service_id',
+		  'service.node_id',
+		  'service.name',
+		  'service.plugin_id',
+		  'service.interval',
+		  'service.timeout'];    
+    my $where = [];
+    my $order = ['service.service_id'];
+
+    my $results = [];
+
+    my $cv = AnyEvent->condvar;
+
+    $self->dbh->select( $table,
+			$fields,
+			$where,
+			$order,
+			sub {
+			    
+			    my ( $dbh, $rows, $rv ) = @_;
+			    
+			    foreach my $row ( @$rows ) {
+
+				push $results, {'service_id' => $row->[0],
+						'node_id' => $row->[1],
+						'name' => $row->[2],
+						'plugin_id' => $row->[3],
+						'interval' => $row->[4],
+						'timeout' => $row->[5]};
+			    }
+			    
+			    $cv->send;
+			} );
+    
+    $cv->recv;
+
+    return $results;
 }
 
 1;
